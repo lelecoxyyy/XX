86e6e3f93c1941cb55e493f4ec399837952f5778
Nadya <wsr3@K218PC5>
2022-04-14 13:12:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] ar = { 0, 1, 2, 3, 4, 5 };
+                int A = 5;
+                ar = ar.Select(x => (x * 2 - A) / ar[0]).ToArray();
+            }
+        }
+    }
+}
